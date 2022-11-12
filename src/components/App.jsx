1c811b6260627682b0098e2e94810f52f8390028
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-11-12 15:06:20 +0100

    add LoadMoreBtn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,29 @@ import React, { Component } from 'react';
 import { SearchBar } from './SearchBar/SearchBar';
 import { fetchImages } from '../helpers/pixabayApi';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-
+import { LoadMore } from './LoadMore/LoadMore';
 class App extends Component {
   state = {
     images: [],
     query: '',
     page: 1,
   };
+
   handleSubmitForm = query => {
-    this.setState({ query });
+    this.setState({ query, page: 1 });
   };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   componentDidUpdate(_, prevState) {
     const { query, page } = this.state;
-    if (prevState.query !== query) {
+    if (prevState.query !== query || prevState.page !== page) {
       fetchImages(query, page).then(data => {
-        this.setState(prev => ({ images: [...prev.images, ...data.hits] }));
+        this.setState(prev => ({
+          images: page === 1 ? data.hits : [...prev.images, ...data.hits],
+        }));
       });
     }
   }
@@ -26,6 +34,7 @@ class App extends Component {
       <>
         <SearchBar onSubmit={this.handleSubmitForm} />
         <ImageGallery images={this.state.images} />
+        <LoadMore onLoadMore={this.handleLoadMore} />
       </>
     );
   }
--- src/helpers/pixabayApi.js
+++ src/helpers/pixabayApi.js
@@ -5,7 +5,7 @@ const KEY = '30220761-19f9286ae78f3e4900bbf1b48';
 
 export const fetchImages = async (query, page) => {
   const { data } = await axios.get(
-    `?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    `?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=30`
   );
   return data;
 };
